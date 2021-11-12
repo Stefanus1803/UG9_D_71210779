3df2e0648834b5456defa6e3bf3d6f368e451b93
Stefanus <stefanus.setyo@ti.ukdw.ac.id>
2021-11-12 12:13:59 +0700

    3_A_71210779.py

---
+++ 3_B_71210779.py
@@ -0,0 +1,9 @@
+#input
+belanja1= input("Masukkan daftar belanja 1: ")
+belanja2= input("Masukkan daftar belanja 2: ")
+tambahan1= input("Tambahkankan data ke daftar belanja 1: ")
+tambahan2= input("Tambahkankan daya ke daftar belanja 2: ")
+
+#output
+print("Daftar belanja 1 adalah ", belanja1, ",", tambahan1)
+print("Daftar belanja 2 adalah ", belanja2, ",", tambahan2)
---
+++ 4_A_71210779.py
@@ -0,0 +1,10 @@
+#input
+a=1
+r=3
+n= 11
+
+#proses
+jumlah= (a*(1-r**n))/(1-r)
+
+#output
+print("Jumlah suku ke", n, "adalah", jumlah)
