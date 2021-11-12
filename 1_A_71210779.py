3d4485dff82ded55b0ee35ce3765f08a06994c80
Stefanus <stefanus.setyo@ti.ukdw.ac.id>
2021-11-12 11:26:41 +0700

    1_A_71210779.py

---
+++ 1_A_71210779.py
@@ -0,0 +1,9 @@
+#hitung luas layangan
+#dengan inputan
+
+d1 = int(input("masukkan d1 : "))
+d2 = int(input("masukkan d2 : "))
+
+luas = 0.5*(d1*d2)
+
+print("luas layangan:", luas, "cm")
---
+++ 2_B_71210779.py
@@ -0,0 +1,12 @@
+#hitung rata rata uang saku
+#dengan input
+
+#uang saku
+anak1 = int(input("Uang saku anak pertama : "))
+anak2 = int(input("Uang saku anak kedua : "))
+anak3 = int(input("Uang saku anak ketiga : "))
+
+rata_rata = (anak1+anak2+anak3) /3
+
+print("Rata-rata dari", anak1,"+",anak2,"+",anak3,"adalah", rata_rata)
+
